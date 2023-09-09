e8011b5d31d8d149ad5b18295f57c65710908dc5
port <108868128+portdeveloper@users.noreply.github.com>
2023-09-09 10:09:24 +0300

    Remove TransactionHash

--- packages/nextjs/components/blockexplorer/index.tsx
+++ packages/nextjs/components/blockexplorer/index.tsx
@@ -3,5 +3,4 @@ export * from "./AddressLogsTab";
 export * from "./AddressStorageTab";
 export * from "./PaginationButton";
 export * from "./SearchBar";
-export * from "./TransactionHash";
 export * from "./TransactionsTable";
